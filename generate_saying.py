69818ca034728e909b78e60f358bce7509261929
Sho Shimauchi <sho.shimauchi@gmail.com>
2016-05-08 11:07:45 +0900

    add sahen verb

--- generate_saying.py
+++ generate_saying.py
@@ -16,19 +16,25 @@ def parse_line(line):
     node = node.next
     converted_arr = []
     nouns = []
+    verbs = []
 
     while node:
         logging.debug("{0}: {1}".format(node.surface, node.feature))
         word = node.surface
         feature = node.feature.split(',')
         word_class = feature[0]
+        word_class_detail = feature[1]
         if word_class == "名詞":
-            nouns.append(word)
-            word = "\"{" + word + "}\""
+            if word_class_detail == "一般":
+                nouns.append(word)
+                word = "\"{" + word + "}\""
+            elif word_class_detail == "サ変接続":
+                verbs.append(word)
+                word = "\"{" + word + "}\""
         converted_arr.append(word)
         node = node.next
 
-    return ''.join(converted_arr), nouns
+    return ''.join(converted_arr), nouns, verbs
 
 
 def generate(input_arr):
@@ -36,12 +42,14 @@ def generate(input_arr):
     # 名詞を"{}"で囲み、最後に全体の文頭・文末に""を付加することで実現。
 
     nouns = []
+    verbs = []
     converted_input = []
 
     for line in input_arr:
-        converted_line, nouns_per_line = parse_line(line)
+        converted_line, nouns_per_line, verbs_per_line = parse_line(line)
         converted_input.append(converted_line)
         nouns += nouns_per_line
+        verbs += verbs_per_line
 
     generated_text = "\"" + '\n'.join(converted_input) + "\""
 
